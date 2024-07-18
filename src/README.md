42c4e3abad2c4290d8893d5cc46236f79561f8fa
Brayden Youngberg <bjyberg1@gmail.com>
2024-07-18 18:46:10 +0100

    Clean and add readme

---
+++ src/README.md
@@ -0,0 +1,5 @@
+Small little function to write COGs from other raster types (geotiff, asc, etc.) with rust.
+It takes a folder path as an input, runs in parallel and seems to be quicker than a similar R function.
+This was done partly to optimize my workflows and as a way for me to learn some basics rust.
+This is my first attempt at rust so if anyone comes across this and has more of an idea of what
+they are doing, I'm super open to feedback.
--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,18 @@ use std::env;
 
 fn create_cog(filepath: &str, folder: &str) {
     println!("Creating COG for {}", filepath);
-    // Path to your raster file
+
     let file_path = filepath;
 
     let basename = Path::new(file_path).file_stem().unwrap().to_str();
 
     let tif_path = format!("{}/{}.tif", folder, basename.unwrap());
 
-    // Open the dataset
     let dataset = Dataset::open(file_path).expect("Failed to open dataset");
 
     let driver = DriverManager::get_driver_by_name("COG").expect("Failed to get driver");
 
     let creation_options = [
-        RasterCreationOption {
-            key: "COMPRESS",
-            value: "LZW"
-        },
         RasterCreationOption {
             key: "COMPRESS",
             value: "LZW"
@@ -42,24 +37,6 @@ fn create_cog(filepath: &str, folder: &str) {
             tif_path,
             &creation_options
         );
-
-    // Create the COG
-    // let tif = driver
-    //     // .create(
-    //     //     "test.tif",
-    //     //     dataset.raster_size().0 as isize,
-    //     //     dataset.raster_size().1 as isize,
-    //     //     dataset.raster_count() as isize);
-    //     .create_with_band_type_with_options::<u8, _>(
-    //         "test.tif",
-    //         dataset.raster_size().0 as isize,
-    //         dataset.raster_size().1 as isize,
-    //         dataset.raster_count() as isize,
-    //         &creation_options,
-    //     );
-
-    // // Print the raster size
-    // println!("Raster size: {} x {}", raster_size.0, raster_size.1);
 }
 
 fn main() {
@@ -68,22 +45,12 @@ fn main() {
 
     let path = &args[1];
 
-    // let path = "/home/bjyberg/bioversity/RangelandData/historical";
-
     let start_time = std::time::Instant::now();
 
     let files: Vec<String> = fs::read_dir(path)
         .expect("Failed to read directory")
         .map(|entry| entry.unwrap().path().display().to_string())
         .collect();
-
-    // let files: Vec<String> = fs::read_dir(path)
-    // .expect("Failed to read directory")
-    // .filter_map(|entry| {
-    //     entry.ok()
-    //         .map(|e| e.path().to_string_lossy().into_owned())
-    // })
-    // .collect();
 
     files.par_iter().for_each(|x| create_cog(x, path));
 
